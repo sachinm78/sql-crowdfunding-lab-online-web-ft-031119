bcfdc9f03636ead5933924ceecb258be53b66711
Sachin Mahajan <sachinm78@gmail.com>
2019-04-09 22:15:03 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,11 +17,11 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
-  FROM users
-  INNER JOIN pledges
-  ON users.id = pledges.user_id
-  GROUP BY users.name
-  ORDER BY users.name;"
+   FROM users
+   INNER JOIN pledges
+   ON users.id = pledges.user_id
+   GROUP BY users.name
+   ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -32,11 +32,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, SUM(pledges.amount)
-  FROM users
-  JOIN pledges ON users.id = pledges.user_id
-  GROUP BY users.name
-  ORDER BY SUM(pledges.amount);"
+  "SELECT users.name, SUM(pledges.amount)
+   FROM users
+   JOIN pledges ON users.id = pledges.user_id
+   GROUP BY users.name
+   ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
